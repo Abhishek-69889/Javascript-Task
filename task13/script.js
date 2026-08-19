03a231bebb973b7e2b16b944efb93a18303ab853
Abhishek-69889 <abhit0396@gmail.com>
2026-08-19 12:41:31 +0530

    Added task 13

---
+++ task13/script.js
@@ -0,0 +1,57 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+let arr =[];
+
+
+btn.addEventListener("click",()=>{
+  let val = inp.value;
+  if(val.length!==0){
+    arr.push(val);
+
+    let div1 = document.createElement("div");
+    let div2 = document.createElement("div");
+    let inp1 = document.createElement("input")
+    let li = document.createElement("li")
+    let button = document.createElement("button");
+
+    ul.appendChild(div1);
+    div1.appendChild(div2);
+    div2.appendChild(inp1);
+    div2.appendChild(li);
+    div1.appendChild(button);
+    
+    div1.classList.add("list-main");
+    inp1.type="checkbox";
+
+    arr.forEach(item=>{
+      li.innerText=item;
+      button.innerText="Delete";
+    });
+
+    button.addEventListener("click",()=>{
+      div1.remove();
+    });
+
+    
+    inp1.addEventListener("click",()=>{
+      if(inp1.checked){
+      console.log("Checked");
+      li.style.color="yellow"
+      li.style.textDecoration="line-through"
+      }else{
+        console.log("not checked");
+        li.style.color="white";
+        li.style.textDecoration="none"
+      };
+    });
+
+    alert(`${val} added to the to do task`);
+    inp.value="";
+
+
+  }else{
+    alert("Enter a valid task Value");
+  }
+})

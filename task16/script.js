7b332aea6934aeee74da520f41407515c6f568c3
Abhishek-69889 <abhit0396@gmail.com>
2026-08-24 20:41:41 +0530

    Task 16 completed

--- task16/script.js
+++ task16/script.js
@@ -1,12 +1,27 @@
 let input = document.querySelector("input");
+let ul = document.querySelector("ul");
 let lis = document.querySelectorAll("li");
-let btn = document.querySelector("button");
-console.log(lis);
 
-btn.addEventListener("click",()=>{
-  lis.forEach(element=>{
-  if(input.value==element){
-    console.log(` ${element}`)
+let items = ["apple", "banana", "cat", "dog", "house", "sun", "moon", "star", "tree", "water", "fire", "book", "pen", "desk", "chair", "shoe", "shirt", "hat", "car", "bus", "train", "road", "city", "town", "hill", "bird", "fish", "milk", "bread", "food"];
+
+// IIFE (Immediately Invoked Function Expression)
+(function (items){
+  items.forEach(element => {
+    let li = document.createElement("li");
+    li.innerText=element;
+    ul.appendChild(li);
+  });
+})(items);
+
+input.addEventListener("input",()=>{
+  const result = items.filter(element=>element.toLocaleLowerCase().includes(input.value.toLocaleLowerCase()))
+  console.log(result);
+  if(result.length>0){
+    ul.innerText="";
+    result.forEach(element => {
+    let li = document.createElement("li");
+    li.innerText=element;
+    ul.appendChild(li);
+    });
   }
-})
 })
